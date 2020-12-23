79b0b948406d081c893e4d6af2baf6942f84547e
Chanh BC <chanhvp.k54.utc@gmail.com>
2020-12-23 09:57:03 +0700

    fix toast

--- app/src/main/java/com/chanhbc/iother/IOther.kt
+++ app/src/main/java/com/chanhbc/iother/IOther.kt
@@ -366,7 +366,7 @@ class IOther private constructor(private val mContext: Context) {
         } else {
             Toast.LENGTH_LONG
         }
-        Toast.makeText(mContext, arrayToString(*s.copyOfRange(1, s.size)), time).show()
+        Toast.makeText(mContext, arrayToString(*s), time).show()
     }
 
     fun about() {
